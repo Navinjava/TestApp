4b814c73a3cf286b5da078bc50283c7fe71a9735
Naveen <sanjava2020@gmail.com>
2021-02-01 11:34:13 -0600

    Initial commit

---
+++ retailerrewards/src/main/java/com/assessment/retailerrewards/RetailerrewardsApplication.java
@@ -0,0 +1,13 @@
+package com.assessment.retailerrewards;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RetailerrewardsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RetailerrewardsApplication.class, args);
+	}
+
+}
---
+++ retailerrewards/src/test/java/com/assessment/retailerrewards/RetailerrewardsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessment.retailerrewards;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RetailerrewardsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
